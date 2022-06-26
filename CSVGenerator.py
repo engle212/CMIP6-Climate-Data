e8082e2914cb76a51388acde91b4ceec1e8361c7
engle212 <engle.212@buckeyemail.osu.edu>
2022-06-26 15:25:25 -0400

    Downloader and CSVGenerator updated to support multiple variables

--- CSVGenerator.py
+++ CSVGenerator.py
@@ -8,20 +8,8 @@ def add_days_to_date(date, days):
     # Calculates and returns a date by adding days to given date
     added_date = pd.to_datetime(date) + timedelta(days=days)
     added_date = added_date.strftime("%Y-%m-%d")
-
     return added_date
 
-def anim(animation):
-    end = False
-    idx = 0
-    num = 0
-    while not end:
-        print(animation[idx % len(animation)], end="\r")
-        idx += 1
-        num += 1
-        time.sleep(0.005)
-        if (idx > len(animation)):
-            end = True
 
 ncDir = 'Downloaded Files'
 outDir = 'Output Files'
@@ -30,7 +18,6 @@ if not os.path.exists(outDir):
     # create output directory
     os.mkdir(outDir)
     print('[output directory created]')
-
 
 all_years = []
 
@@ -49,47 +36,66 @@ date_range = pd.date_range(start=str(year_start)+'-01-01', end=str(year_end)+'-1
 # create and populate data structure for locations csv data
 locations = pd.read_csv('locations_sub.csv')
 
-
 all_years.sort()
 
-for yr in all_years:
-    # read data
-    inFilePath = os.path.join(ncDir, str(yr)+'.nc')
-    data = Dataset(inFilePath, 'r')
+df = pd.DataFrame(columns=['YEAR', 'DOY', 'Date'])
 
-    lat = data.variables['lat'][:]
-    lon = data.variables['lon'][:]
+for model in os.listdir(ncDir): # go through each model folder
+    modDir = os.path.join(ncDir, model)
+    for type in os.listdir(modDir): # go through each type folder
+        typeDir = os.path.join(modDir, type)
+        for var in os.listdir(typeDir): # go through each var folder
+            varDir = os.path.join(typeDir, var)
+            # read each file in var folder
+            for yr in all_years:
+                # read data
+                inFilePath = os.path.join(varDir, str(yr)+'.nc')
+                data = Dataset(inFilePath, 'r')
 
-    # Access precipitation data
-    pr = data.variables['pr']
+                start = str(yr) + '-01-01'
+                end = str(yr) + '-12-31'
 
-    start = str(yr) + '-01-01'
-    end = str(yr) + '-12-31'
+                d_range = pd.date_range(start=start, end=end, freq='D')
 
-    df = pd.DataFrame(0.0, columns=['pr'], index=date_range)
+                yearChunk = pd.DataFrame(0.0, columns=['YEAR', 'DOY', 'Date'], index=d_range)
 
-    d_range = pd.date_range(start=start, end=end, freq='D')
+                lat = data.variables['lat'][:]
+                lon = data.variables['lon'][:]
 
-    for index, row in locations.iterrows():
-        locLat = row[0]
-        locLon = row[1]
-        name = row[2]
-        column = pd.DataFrame(0.0, columns=[name], index=date_range)
-        # squared differences of lat and lon
-        sq_diff_lat = (lat - locLat)**2
-        sq_diff_lon = (lon - locLon)**2
-        # get index of minimum distance
-        min_index_lat = sq_diff_lat.argmin()
-        min_index_lon = sq_diff_lon.argmin()
+                # Access data for current variable
+                varData = data.variables[var]
 
-        for t_index in np.arange(0, len(d_range)):
-            print('Recording value for: ' + str(d_range[t_index]) + ' @ ' + name)
-            column.loc[d_range[t_index]]['pr'] = pr[t_index, min_index_lat, min_index_lon]
+                doy = np.arange(1, len(d_range) + 1)
 
-        # concat df with column
-        df = pd.concat([df, column], axis=1, ignore_index=False)
-    outFileName = 'pr_'+str(year_start)+'_'+str(year_end)+'.csv'
-    outFilePath = os.path.join(outDir, outFileName)
+                # populate doy and year column
+                for x in d_range:
+                    yearChunk.at[x, 'DOY'] = str(doy[d_range.get_loc(x)])
+                    yearChunk.at[x, 'YEAR'] = str(yr)
 
+                for index, row in locations.iterrows():
+                    locLat = row[0]
+                    locLon = row[1]
+                    name = row[2]
+                    column = pd.DataFrame(0.0, columns=[name], index=d_range)
+                    # squared differences of lat and lon
+                    sq_diff_lat = (lat - locLat)**2
+                    sq_diff_lon = (lon - locLon)**2
+                    # get index of minimum distance
+                    min_index_lat = sq_diff_lat.argmin()
+                    min_index_lon = sq_diff_lon.argmin()
 
-    df.to_csv(outFilePath)
+                    for t_index in np.arange(0, len(d_range)):
+                        print('Recording value for: ' + str(d_range[t_index]) + ' @ ' + name)
+                        column.at[d_range[t_index], name] = varData[t_index, min_index_lat, min_index_lon]
+
+                    # concat df with column
+                    yearChunk = pd.concat([yearChunk, column], axis=1, ignore_index=False).copy()
+                df = pd.concat([df, yearChunk], axis=0, ignore_index=False).copy()
+
+                outFileName = 'pr_'+str(year_start)+'_'+str(year_end)+'.csv'
+                outFilePath = os.path.join(outDir, outFileName)
+
+                df['Date'] = df.index
+                df.rename(columns={'index':'Date'})
+
+                df.to_csv(outFilePath, index=False)
--- Downloader.py
+++ Downloader.py
@@ -1,14 +1,40 @@
 import requests
 import os
+import pandas as pd
 
 def requestFromServer(reqURL):
     try:
+        print('**')
         r = requests.get(reqURL)
-    except requests.exceptions.Timeout:
+        print('[]')
+    except requests.exceptions.RequestException as e:
         # retry
         print('[Retrying server request]')
         r = requestFromServer(reqURL)
     return r
+
+def downloadDataRange(directory, model, type, var, startYear, endYear, north, west, east, south):
+    yRange = range(int(startYear), int(endYear) + 1)
+
+    for year in yRange:
+        print(var)
+        # assemble request URL using info
+        url = ''.join(['https://ds.nccs.nasa.gov/thredds/ncss/AMES/NEX/GDDP-CMIP6/', str(model), '/', str(type), '/r1i1p1f1/', str(var), '/', str(var), '_day_', str(model), '_', str(type), '_r1i1p1f1_gn_', str(year), '.nc?var=', str(var), '&north=', str(north), '&west=', str(west), '&east=', str(east), '&south=', str(south), '&disableProjSubset=on&horizStride=1&time_start=', str(year), '-01-01T12%3A00%3A00Z&time_end=', str(year), '-12-31T12%3A00%3A00Z&timeStride=1&addLatLon=true'])
+
+        req = requestFromServer(url)
+
+        fileName = str(var) + '_day_' + model + '_' + str(type) + '_r1i1p1f1_gn_' + str(year) + '.nc'
+
+        filePath = os.path.join(directory, model, type, var, fileName)
+
+        with open(filePath, 'wb') as f:
+            f.write(req.content)
+        print('[DOWNLOAD COMPLETED]......' + fileName)
+
+
+# Create all directories based on info from DataSpecifications.csv
+specs = pd.read_csv('DataSpecifications.csv', header=0)
+specs.fillna(0, inplace=True)
 
 outDir = 'Downloaded Files'
 if not os.path.exists(outDir):
@@ -16,42 +42,55 @@ if not os.path.exists(outDir):
     os.mkdir(outDir)
     print('[output directory created]')
 
-north = 42.337
-west = -85.301
-east = -82.654
-south = 40.368
+for model in specs.loc[:]['Models']:
+    if model != 0:
+        modelDir = os.path.join(outDir, model)
+        if not os.path.exists(modelDir):
+            # create model directory
+            os.mkdir(modelDir)
+        for type in specs.loc[:]['Types']:
+            if type != 0:
+                typeDir = os.path.join(modelDir, type)
+                if not os.path.exists(typeDir):
+                    # create type directory
+                    os.mkdir(typeDir)
+                for var in specs.loc[:]['Variables']:
+                    if var != 0:
+                        varDir = os.path.join(typeDir, var)
+                        if not os.path.exists(varDir):
+                            # create var directory
+                            os.mkdir(varDir)
+                        print(varDir)
+                        # request corresponding data
+                        if model == 'historical':
+                            start = specs.at[0, 'hStart']
+                            end = specs.at[0, 'hEnd']
+                        else:
+                            start = specs.at[0, 'StartYear']
+                            end = specs.at[0, 'EndYear']
+                        downloadDataRange(outDir, model, type, var, start, end, specs.at[0, 'north'], specs.at[0, 'west'], specs.at[0, 'east'], specs.at[0, 'south'])
 
-print('Welcome!')
-print('...')
-print('')
-print('')
+#north = 42.337
+#west = -85.301
+#east = -82.654
+#south = 40.368
+
+#print('Welcome!')
+#print('...')
+#print('')
+#print('')
 # get info for request URL from user
-model = input('Enter the model: ')
+#model = input('Enter the model: ')
 
-type = input('Enter the type of climate data: ')
+#type = input('Enter the type of climate data: ')
 
-var = input('Enter the variable name: ')
+#var = input('Enter the variable name: ')
 
-start = input('Enter start year: ')
-end = input('Enter end year: ')
+#start = input('Enter start year: ')
 
-print('')
-print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
-print('')
+#end = input('Enter end year: ')
 
-yRange = range(int(start), int(end) + 1)
 
-for year in yRange:
-    # assemble request URL using info
-    url = ''.join(['https://ds.nccs.nasa.gov/thredds/ncss/AMES/NEX/GDDP-CMIP6/', str(model), '/', str(type), '/r1i1p1f1/', str(var), '/', str(var), '_day_', str(model), '_', str(type), '_r1i1p1f1_gn_', str(year), '.nc?var=pr&north=', str(north), '&west=', str(west), '&east=', str(east), '&south=', str(south), '&disableProjSubset=on&horizStride=1&time_start=', str(year), '-01-01T12%3A00%3A00Z&time_end=', str(year), '-12-31T12%3A00%3A00Z&timeStride=1&addLatLon=true'])
-
-    req = requestFromServer(url)
-
-    fileName = str(var) + '_day_' + model + '_' + str(type) + '_r1i1p1f1_gn_' + str(year) + '.nc'
-
-    filePath = os.path.join(outDir, fileName)
-
-    with open(filePath, 'wb') as f:
-        f.write(req.content)
-
-    print('[DOWNLOAD COMPLETED]......' + fileName)
+#print('')
+#print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+#print('')
